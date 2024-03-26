0253dc02ca3abc0d2181da2d91e76f8e2d902840
aravix <aravixdev@gmail.com>
2024-03-26 11:15:55 +0100

    Refactor generation

--- src/worldgen/world.rs
+++ src/worldgen/world.rs
@@ -21,12 +21,12 @@ impl Default for VoxelWorld {
             world_noise: Fbm::<Perlin>::new(42069).set_octaves(6),
             verticality: Perlin::new(42069),
             spline_points: Spline::from_vec(vec![
-                splines::Key::new(-1., 0.6, splines::Interpolation::Cosine),
-                splines::Key::new(-0.9, 0.7, splines::Interpolation::Cosine),
-                splines::Key::new(0., 0.8, splines::Interpolation::Cosine),
-                splines::Key::new(0.5, 0.85, splines::Interpolation::Cosine),
-                splines::Key::new(0.8, 0.9, splines::Interpolation::Cosine),
-                splines::Key::new(0.9, 1., splines::Interpolation::Cosine),
+                splines::Key::new(-1., 0.6, splines::Interpolation::Linear),
+                splines::Key::new(-0.9, 0.7, splines::Interpolation::Linear),
+                splines::Key::new(0., 0.8, splines::Interpolation::Linear),
+                splines::Key::new(0.5, 0.85, splines::Interpolation::Linear),
+                splines::Key::new(0.8, 0.9, splines::Interpolation::Linear),
+                splines::Key::new(0.9, 1., splines::Interpolation::Linear),
                 splines::Key::new(1.1, 1.5, splines::Interpolation::default())
             ]),
             chunks: HashMap::new(),
@@ -40,24 +40,22 @@ impl VoxelWorld {
     pub fn generate_chunk(&mut self, chunk_pos: IVec3) {
         let mut chunk = Chunk::new(chunk_pos);
 
-        let new_pos = chunk.position * CHUNK_SIZE;
+        let world_pos = chunk_pos * CHUNK_SIZE;
 
         for x in 0..CHUNK_SIZE {
-            let wx = x + new_pos.x;
-            for z in 0..CHUNK_SIZE {
-                let wz = z + new_pos.z;
+            for y in 0..CHUNK_SIZE {
+                for z in 0..CHUNK_SIZE {
+                    let wx = (x + world_pos.x) as f64;
+                    let wy = (y + world_pos.y) as f64;
+                    let wz = (z + world_pos.z) as f64;
 
-                let some_pos = Vec2::new(wx as f32, wz as f32) * 0.01;
-                let mut sample = self.world_noise.get([some_pos.x as f64, some_pos.y as f64]);
+                    let nx = wx / ((CHUNK_SIZE_F64 * 4.0) * (CHUNK_SIZE_F64 * 0.1));
+                    let nz = wz / ((CHUNK_SIZE_F64 * 4.0) * (CHUNK_SIZE_F64 * 0.1));
 
-                let vert = self.verticality.get([some_pos.x as f64, some_pos.y as f64]);
+                    let mut sample = self.world_noise.get([nz, nx]);
+                    let vert = self.verticality.get([nx, nz]);
+                    sample *= (CHUNK_SIZE_F64 * 4.0) * self.spline_points.sample(vert).unwrap();
 
-                sample *= (CHUNK_SIZE_F64 * 4.0) * self.spline_points.sample(vert).unwrap();
-
-                // Determine which voxels are going to be solid
-                let local_height = i32::min((sample as i32) - new_pos.y, CHUNK_SIZE);
-
-                for y in 0..CHUNK_SIZE {
                     let index = voxel_index(x, y, z);
                     chunk.voxels[index].local_position = IVec3::new(x, y, z);
                     chunk.voxels[index].world_position = IVec3::new(
@@ -66,21 +64,21 @@ impl VoxelWorld {
                         z + chunk.position.z * CHUNK_SIZE,
                     );
 
-                    if y < local_height {
-                        chunk.voxels[index].voxel_type = VoxelType::GRASS;
+                    if sample < wy && wy <= 20.0 {
+                        chunk.voxels[index].voxel_type = VoxelType::WATER;
+                        continue;
+                    }
+
+                    if wy <= sample {
+                        if wy <= 30.0 {
+                            chunk.voxels[index].voxel_type = VoxelType::GRASS;
+                            continue;
+                        } else {
+                            chunk.voxels[index].voxel_type = VoxelType::STONE;
+                            continue;
+                        }
                     }
                 }
-            }
-        }
-
-        // Test
-        for voxel in &mut chunk.voxels {
-            if !voxel.voxel_type.is_visible() && voxel.world_position.y < 10 {
-                voxel.voxel_type = VoxelType::WATER;
-            }
-
-            if voxel.voxel_type.is_visible() && voxel.world_position.y > 23 {
-                voxel.voxel_type = VoxelType::STONE;
             }
         }
 
